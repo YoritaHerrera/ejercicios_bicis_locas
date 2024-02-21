94fcce63689f2fe529943e3b895f61955365be92
YoritaHerrera <157725589+YoritaHerrera@users.noreply.github.com>
2024-02-21 11:33:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function validateForm () {
+  // your code here
+  //1. traemos los valores del formulario
+  const name =$("#name").validateForm
+  //2.Validar el largo del nombre
+  if (name.lenght < 3){
+    alert ("El nombre es muy corto");
+  }
+  
+}
